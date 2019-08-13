01163b405d4753ceacaebcb67e33f33dcc89ae72
sebaegana <sebaegana@gmail.com>
2019-08-13 01:25:57 -0400

    20190812_v2

--- build_site.R
+++ build_site.R
@@ -1,2 +1,6 @@
 library("rmarkdown")
 rmarkdown::render_site()
+
+#git commit -a -m "Added information on PFKM"
+#git remote add origin git@github.com:USERNAME/GitExercise.git
+#git push -u origin master
