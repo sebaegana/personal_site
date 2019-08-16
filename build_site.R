3a29b7571a17db0d6d359536457dfd693912e3b1
sebaegana <sebaegana@gmail.com>
2019-08-16 00:38:37 -0400

    add buttons

--- build_site.R
+++ build_site.R
@@ -1,6 +1,4 @@
 library("rmarkdown")
 rmarkdown::render_site()
 
-#git commit -a -m "Added information on PFKM"
-#git remote add origin git@github.com:USERNAME/GitExercise.git
-#git push -u origin master
+# http://nickstrayer.me/RMarkdown_Sites_tutorial/
